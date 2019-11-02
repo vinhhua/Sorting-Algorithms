74ae91cf641ccbbb33d79f84fe5de8dfa62c4171
huaphuvinh <huaphuvinh@gmail.com>
2019-11-01 20:43:07 -0700

    insertion sort

--- src/com/VinhHua/Main.java
+++ src/com/VinhHua/Main.java
@@ -6,8 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 	    int[] array = {2, -15, 23, -4, 3, 5, 7, 16};
-	    bubbleSort(array);
+	    int[] array2 = {3, -2, 4, 6, 12, 25, 5, -12};
+ 	    bubbleSort(array);
+ 	    insertionSort(array2);
         System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array2));
 
     }
 
@@ -18,6 +21,18 @@ public class Main {
                     swap(array, j, j + 1);
                 }
             }
+        }
+    }
+
+    private static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i;
+            while (j > 0 && array[j - 1] > key) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = key;
         }
     }
 
