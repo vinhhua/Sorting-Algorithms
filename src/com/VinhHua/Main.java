46c1f80feddd4fb3a8ab6b66f4981cfbe6bebaf5
huaphuvinh <huaphuvinh@gmail.com>
2019-11-02 23:48:14 -0700

    comment

--- src/com/VinhHua/Main.java
+++ src/com/VinhHua/Main.java
@@ -39,15 +39,15 @@ public class Main {
 
     private static int binarySearch(int[] array, int value) {
         int left = 0;
-        int right = array.length;
+        int right = array.length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
             if (array[mid] == value) {
                 return mid;
-            } else if (array[mid] > value) {
-                right = mid;
-            } else {
+            } else if (array[mid] < value) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
         return -1;
