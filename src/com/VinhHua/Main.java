5d12e8357cb8389a75e6bf6534f582f25847c991
huaphuvinh <huaphuvinh@gmail.com>
2020-01-17 21:46:27 -0800

    added merge sort

--- src/com/VinhHua/Main.java
+++ src/com/VinhHua/Main.java
@@ -137,6 +137,33 @@ public class Main {
         return j;
     }
 
+    public static void mergeSort(int[] array, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid, end);
+        merge(array, start, mid, end);
+    }
+
+    private static void merge(int[] array, int start, int mid, int end) {
+        if (array[mid - 1] <= array[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = array[i] <= array[j] ? array[i++] : array[j++];
+        }
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+    }
+
 
     private static void swap(int[] array, int i, int j) {
         if (i == j) {
